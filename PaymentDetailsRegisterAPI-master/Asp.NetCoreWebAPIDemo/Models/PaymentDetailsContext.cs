acec251eebbb5c4d393be324b9cb61185582313b
aliya1628 <54244127+aliya1628@users.noreply.github.com>
2019-10-16 16:53:44 +0530

    Initial commit

---
+++ PaymentDetailsRegisterAPI-master/Asp.NetCoreWebAPIDemo/Models/PaymentDetailsContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asp.NetCoreWebAPIDemo.Models
+{
+    public class PaymentDetailsContext:DbContext
+    {
+        /*options: In this parameters we have to pass information like DBProvider(i.e. SQL SERVER,MYSQL etc), Connection strings
+          how we can pass values those values into this constructor parameter for that we will use "Dependency Injection" */
+        public PaymentDetailsContext(DbContextOptions<PaymentDetailsContext> options):base(options)
+        {
+
+        }
+        public DbSet<PaymentDetail> PaymentDetails { get; set; }
+    }
+}
---
+++ PaymentDetailsRegisterAPI-master/Asp.NetCoreWebAPIDemo/Startup.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Asp.NetCoreWebAPIDemo.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json.Serialization;
+
+namespace Asp.NetCoreWebAPIDemo
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc()
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_2)
+                 //by default json uses camelcase for key names --- this code will nullyfies the extra conversion while returning json data
+                 .AddJsonOptions(Options => {
+                     var resolver = Options.SerializerSettings.ContractResolver;
+                     if (resolver != null)
+                         (resolver as DefaultContractResolver).NamingStrategy = null;
+                 });
+
+            //Dependency Injection is done here
+            /*whenever we create a controller with constructor parameter of the type PaymentDetailsContext, this IServiceCollection 
+             property "services" will provide the new Instance of this "PaymentDetailsContext" into the constructor parameter "context"*/
+            services.AddDbContext<PaymentDetailsContext>(Options =>
+            Options.UseSqlServer(Configuration.GetConnectionString("DevConnection")));
+
+            services.AddCors();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseCors(options => options.WithOrigins("http://localhost:4200")
+            .AllowAnyMethod()
+            .AllowAnyHeader());
+
+            app.UseMvc();
+        }
+    }
+}
